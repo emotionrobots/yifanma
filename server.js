063fd8865038b391b56bc6dfe87945c1f2a613c7
michael <hanteedoc137@gmail.com>
2021-08-19 16:13:51 -0500

    added https

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const app = require('./index')
 
-app.listen(9000, (err) => {
+const httpsServer = https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/pplcnt-web.e-motion.ai/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/pplcnt-web.e-motion.ai/fullchain.pem'),
+  }, app);
+
+httpsServer.listen(9000, (err) => {
     if(err) throw err
     console.log('Server running in http://127.0.0.1:9000')
 })
