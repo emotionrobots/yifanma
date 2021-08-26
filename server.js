5c46bc677e83f89e495eca894599e0f93fcab793
michael <hanteedoc137@gmail.com>
2021-08-25 21:46:57 -0500

    added test files for https testing

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ const app = require('./index')
 const https = require('https')
 const fs = require('fs')
 
-const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/pc3-backend.e-motion.ai/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/pc3-backend.e-motion.ai/fullchain.pem')
-  }
+// const options = {
+//     key: fs.readFileSync('/etc/letsencrypt/live/pc3-backend.e-motion.ai/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/pc3-backend.e-motion.ai/fullchain.pem')
+//   }
 
-httpsServer = https.createServer(options)
+// httpsServer = https.createServer(options, app)
 
-httpsServer.listen(9000, (err) => {
-    if(err) throw err
-    console.log('Server running in http://127.0.0.1:9000')
-})
+// httpsServer.listen(9000, (err) => {
+//     if(err) throw err
+//     console.log('Server running in http://127.0.0.1:9000')
+// })
 
 // const httpsServer = https.createServer({
 //     key: fs.readFileSync('/etc/letsencrypt/live/pc3-backend.e-motion.ai/privkey.pem'),
 //     cert: fs.readFileSync('/etc/letsencrypt/live/pc3-backend.e-motion.ai/fullchain.pem'),
 //   }, app);
 
-// app.listen(9000, (err) => {
-//     if(err) throw err
-//     console.log('Server running in http://127.0.0.1:9000')
-// })
+app.listen(9000, (err) => {
+    if(err) throw err
+    console.log('Server running in http://127.0.0.1:9000')
+})
