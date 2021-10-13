827138e88bd1b2ac99bcc3681a562aa27aa0c9a4
michael <hanteedoc137@gmail.com>
2021-10-13 17:06:37 -0500

    untrack files 2

--- routes/data.router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const dataService = require('../services/data.service')
-
-router.get('/', dataService.getData)
-
-module.exports = router
